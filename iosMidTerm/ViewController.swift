3ae23e898971fcddd08f0cb8233e898da6878786
Dyana George <dyana.t.s.george@gmail.com>
2018-10-18 20:47:36 -0400

    buttons added

--- iosMidTerm/ViewController.swift
+++ iosMidTerm/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //makes vars
+    var listOne
+    var listTwo
+    var listThree
+    var listFour
+    var listFive
+    var qunOne : UILabel!
+    var qunTwo : UILabel!
+    var qunThree : UILabel!
+    var qunFour : UILabel!
+    var qunFive : UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +35,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func submitPressed_btn(_ sender: UIButton) {
+    }
+    
+    
+    weak var cancelPressed_btn: UIButton! {
+        qunOne.text = ""
+        qunTwo.text = ""
+        qunThree.text = ""
+        qunFour.text = ""
+        qunFive.text = ""
+    }
 }
 
+
+
+//func adjustUITextViewHeight(arg : UITextView)
+//{
+//    arg.translatesAutoresizingMaskIntoConstraints = true
+//    arg.sizeToFit()
+//    arg.scrollEnabled = false
+//}
